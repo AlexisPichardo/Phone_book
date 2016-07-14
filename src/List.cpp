d1771bd06eaa5f308dbe27912dc2fd3b787dc4d8
AlexisPichardo <20142129@itla.edu.do>
2016-07-14 06:19:24 -0700

    Tarea no. 4 Jose ALexis

--- src/List.cpp
+++ src/List.cpp
@@ -18,20 +18,76 @@ List::List(): _first(NULL), _last(NULL)
 
 void List::add(Element* element)
 {
-	//TODO: Implement this method
-	cout << "You should write the code for adding " << element -> _name << endl;
+	system("cls");
+	Contacto contacto;
+	ofstream archivo;
+	string nombre_archivo = "Agenda.txt";
+	archivo.open(nombre_archivo.c_str(), ios::app);
+	contacto.id = id;
+	cin.ignore();
+	cout << "Agregar Contacto" << endl;
+	cout << endl << "Nombre: ";
+	cin.getline(contacto.nombre, 30);
+	cout << endl << "Numero: ";
+	cin.getline(contacto.telefono, 11);
+
+	archivo.write((char *)&contacto, sizeof(contacto));
+
+	archivo.close();
 
 }
 
 void List::remove(int index)
 {
-	//TODO: Implement this method
-	cout << "You should write the code for removing the index " << index << endl;
+	fstream leer_archivo("Agenda.txt");
+	ofstream archivo("borrar.txt");
+	Contacto contacto;
+	leer_archivo.read((char *)&contacto, sizeof(contacto));
+
+	if (leer_archivo.fail())
+	{
+		cout << " Ocurrio un ploblema, intentando abrir la agenda :(" << endl;
+		exit(1);
+	}
+
+	while (!leer_archivo.eof())
+	{
+
+		if (contacto.id != id)
+		{
+
+			archivo.write((char *)&contacto, sizeof(contacto));
+		}
+		leer_archivo.read((char *)&contacto, sizeof(contacto));
+	}
+	cout << endl;
+	leer_archivo.close();
+	archivo.close();
+
+	remove("Agenda.txt");
+	rename("borrar.txt", "Agenda.txt");
 }
 
 void List::modify(int index, std::string name)
 {
-	//TODO: Implement this method
-	cout << "You should write the code for modifying the content for index " << index << endl;
+	ifstream leer_archivo("Agenda.txt");
+	Contacto contacto;
+
+	leer_archivo.read((char *)&contacto, sizeof(contacto));
+	if (leer_archivo.fail())
+	{
+		cout << " Ocurrio un ploblema, intentando abrir la agenda :(" << endl;
+		exit(1);
+	}
+	cout << "id   Nombre     Telefono" << endl;
+	while (!leer_archivo.eof())
+	{
+
+		cout << endl << contacto.id << " -> " << contacto.nombre << "   " << contacto.telefono << endl;
+		leer_archivo.read((char *)&contacto, sizeof(contacto));
+
+	}
+	cout << endl;
+	leer_archivo.close();
 
 }
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -71,20 +71,76 @@ void Menu::routeAction(int option)
 
 void Menu::listContacts()
 {
-	//TODO: Implement this
-	cout << "Implement  Menu::listContacts" << endl;
+	ifstream leer_archivo("Agenda.txt");
+	Contacto contacto;
+
+	leer_archivo.read((char *)&contacto, sizeof(contacto));
+	if (leer_archivo.fail())
+	{
+		cout << " Ocurrio un ploblema, intentando abrir la agenda :(" << endl;
+		exit(1);
+	}
+	cout << "id   Nombre     Telefono" << endl;
+	while (!leer_archivo.eof())
+	{
+
+		cout << endl << contacto.id << " -> " << contacto.nombre << "   " << contacto.telefono << endl;
+		leer_archivo.read((char *)&contacto, sizeof(contacto));
+
+	}
+	cout << endl;
+	leer_archivo.close();
 }
 
 void Menu::addContacts()
 {
-	//TODO: Implement this
-	cout << "Implement  Menu::addContacts" << endl;
+	system("cls");
+	Contacto contacto;
+	ofstream archivo;
+	string nombre_archivo = "Agenda.txt";
+	archivo.open(nombre_archivo.c_str(), ios::app);
+	contacto.id = id;
+	cin.ignore();
+	cout << "Agregar Contacto" << endl;
+	cout << endl << "Nombre: ";
+	cin.getline(contacto.nombre, 30);
+	cout << endl << "Numero: ";
+	cin.getline(contacto.telefono, 11);
+
+	archivo.write((char *)&contacto, sizeof(contacto));
+
+	archivo.close();
 }
 
 void Menu::removeContacts()
 {
-	//TODO: Implement this
-	cout << "Implement  Menu::removeContacts" << endl;
+	fstream leer_archivo("Agenda.txt");
+	ofstream archivo("borrar.txt");
+	Contacto contacto;
+	leer_archivo.read((char *)&contacto, sizeof(contacto));
+
+	if (leer_archivo.fail())
+	{
+		cout << " Ocurrio un ploblema, intentando abrir la agenda :(" << endl;
+		exit(1);
+	}
+
+	while (!leer_archivo.eof())
+	{
+
+		if (contacto.id != id)
+		{
+
+			archivo.write((char *)&contacto, sizeof(contacto));
+		}
+		leer_archivo.read((char *)&contacto, sizeof(contacto));
+	}
+	cout << endl;
+	leer_archivo.close();
+	archivo.close();
+
+	remove("Agenda.txt");
+	rename("borrar.txt", "Agenda.txt");
 }
 
 void Menu::modifyContacts()
